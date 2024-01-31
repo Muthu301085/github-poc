7c999d17caa160b0a452a5693087043390e81729
Muthu301085 <157270320+Muthu301085@users.noreply.github.com>
2024-01-31 15:31:17 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/com/rds/demo/DemoApplication.java
@@ -0,0 +1,39 @@
+package com.rds.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	public Object callRDSApi(Order order) {
+
+		try {
+			ResponseEntity<String> iFastOrderResponse = restTemplate.exchange(graphQLProperties.getUrl(), HttpMethod.POST, httpEntityOf(buildNewIfastOrder(order, IFastConstants.REQUEST_TYPE_ORDER)), String.class);
+			log.info("IFast Order Response  :: " + iFastOrderResponse);
+			ensureThatRequiredBodyIsPresent(iFastOrderResponse);
+			DataResponse iFastGnOrderResponse = objectMapper.readValue(iFastOrderResponse.getBody(), new TypeReference<DataResponse>() {
+			});
+			ensureThatRequiredObjectForOrderIsNotNull(iFastGnOrderResponse);
+
+			//TODO Remove logger once Graphql logging added to Kibana
+			log.info("IFast Response  :: " + iFastGnOrderResponse.getData().getOrder());
+
+			return iFastGnOrderResponse;
+		} catch (Exception e) {
+			log.error("Exception occured while calling IFast : " + e.getMessage());
+			throw new KeaRuntimeException(Optional.of(e)
+					.map(InvestorApiErrors::investorErrorResponseOf)
+					.map(HttpEntity::getBody)
+					.map(ErrorMessage::getErrorCode)
+					.map(ErrorCode::getCode)
+					.orElse(UNKNOWN_ERROR.getCode().getCode()));
+		}
+	}
+}
